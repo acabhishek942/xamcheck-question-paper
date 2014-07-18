6cadcbc704a08edca4368b57bc17730d0ef7a0f4
Abhyuday Pratap <abhyuday.cool@gmail.com>
2014-07-18 11:45:44 +0530

    imported constants in forms

--- Question_paper/forms.py
+++ Question_paper/forms.py
@@ -1,34 +1,14 @@
 from django import forms
+import models
 from .models import PostAd
-
-SCHOOLS = (
-    ('CPS', 'Chirec Public School'),
-    ('OPS', 'Oakridge Public School'),
-    ('KPS', 'Kenndy Public School'),
-    ('SPS', 'Silverstone Public School'),
-)
-CLASS = (
-    ('C1', 'Class 1'),
-    ('C2', 'Class 3'),
-    ('C3', 'Class 4'),
-    ('C4', 'Class 5'),
-    ('C5', 'Class 6'),
-)
-SUBJECTS = (
-    ('ENG', 'English'),
-    ('MATHS', 'Mathematics'),
-    ('SS', 'Social Science'),
-    ('SCI', 'Science'),
-    ('TEL', 'Telugu'),
-    ('HIN', 'Hindi'),
-)
 
 
 class PostAdForm(forms.ModelForm):
     error_css_class = 'error'
 
-    category = forms.ChoiceField(choices=SCHOOLS, required=True)
-    location = forms.ChoiceField(choices=CLASS, required=True)
+    school_name = forms.ChoiceField(choices=models.SCHOOLS, required=True)
+    class_name = forms.ChoiceField(choices=models.CLASS, required=True)
+    subject_name = forms.ChoiceField(choices=models.SUBJECTS, required=True)
 
     class Meta:
         model = PostAd
